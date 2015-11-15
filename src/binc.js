c8c95e987bfe14af407137fd7f47d2f8362e7769
Mark Wales <mark@smallhadroncollider.com>
2015-11-15 11:36:40 +0000

    updated binc.js to create ~/.binc folder if it doesn't exist

--- src/binc.js
+++ src/binc.js
@@ -5,4 +5,9 @@
 const map = require("./map");
 const handle = require("./handle");
 
-map.forEach(handle);
+const mkdirp = require("mkdirp");
+const home = process.env.HOME;
+
+mkdirp(`${home}/.binc`, function(err) {
+    return err ? console.log(err) : map.forEach(handle);
+});
